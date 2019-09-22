e137cda81638719c78fd9fb4690edaecb47d760f
tchakradhar <tchakradhar2010@gmail.com>
2019-09-22 17:46:56 +0530

    Migration Tables View

--- application/views/static/migration_process.php
+++ application/views/static/migration_process.php
@@ -31,7 +31,10 @@
 						<?php if( !empty($this->session->userdata('selected_tables'))){
 							$schema_list_sel = json_decode($this->session->userdata('selected_tables'));
 							foreach ( $schema_list_sel  as $key => $value) { //echo $value;
-								echo "<span class=' d-block py-2 '>".$value->id."</span>";
+								echo "<form method='Post' action=''>
+								<input type='hidden' name='schema' value='$value->label'>
+								<input type='hidden' name='table' value='$value->id'>
+								<button class='d-block py-2 migrate_button'>".$value->id."</button></form>";
 								}
 							} 
 						?>	
@@ -40,7 +43,7 @@
 				<td>
 					<div class="d-block">
 						<?php foreach ($target as $row) {
-								echo "<span class=' d-block py-2 '>PUBLIC</span>";
+								echo "<span class=' d-block py-2'>PUBLIC</span>";
 						} ?>	
 					</div>
 				</td>
@@ -57,6 +60,149 @@
 			</tr>
 		</table>
 	</div>
-		<a  class="text-decoration-none btn btn-success text-light" href="<?php echo base_url();?>migration">Migration </a>
+		
 </div>
-    
+<?php if($database_list == 'Amazon RedShift'){?>
+<div class="col-md-10 mt-4 px-5 table_view <?php if(count($Table_Details)>0){echo 'd-block';} ?>">
+<table class=" table table-striped">
+			<tr scope="col" class="table-danger">
+				<th>Source Table</th>
+				<th>Source Column</th>
+				<th>Source Data Type</th>
+				<th>Target Table</th>
+				<th>Target Column</th>
+				<th>Target Data Type</th>
+			</tr>
+			<tr scope="col" class="table table-striped" >
+				<td>
+					<div class="d-block">
+						<?php foreach($Table_Details  as $k) {
+								echo "<span class=' d-block py-2 '>".$k['table_name']."</span>";
+							}
+						?>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+					<?php foreach ( $Table_Details  as $k) {
+								echo "<span class=' d-block py-2 '>".$k['column_name']."</span>";
+								}
+						?>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+					<?php foreach ( $Table_Details  as $k) {
+								echo "<span class=' d-block py-2 '>".$k['data_type']."</span>";
+								}
+						?>	
+					</div>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+						<?php foreach($Table_Details  as $k) {
+								echo "<input class=' d-block py-2 ' value=".$k['table_name'].">";
+							}
+						?>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+					<?php foreach ( $Table_Details  as $k) {
+								echo "<input class=' d-block py-2 ' value=".$k['column_name'].">";
+								}
+						?>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+					<?php foreach ( $Table_Details  as $k) {
+								echo "<input class=' d-block py-2 ' value=".$k['data_type'].">";
+								}
+						?>	
+					</div>	
+					</div>
+				</td>
+			</tr>
+		</table>
+</div>
+<?php }else{?>
+	<div class="col-md-10 mt-4 px-5 table_view <?php if(count($Table_Details)>0){echo 'd-block';} ?>">
+<table class=" table table-striped">
+			<tr scope="col" class="table-danger">
+				<th>Source Table</th>
+				<th>Source Column</th>
+				<th>Source Data Type</th>
+				<th>Target Table</th>
+				<th>Target Column</th>
+				<th>Target Data Type</th>
+			</tr>
+			<tr scope="col" class="table table-striped" >
+				<td>
+					<div class="d-block">
+						<?php foreach($Table_Details  as $k) {
+								echo "<span class=' d-block py-2 '>".$k['TABLE_NAME']."</span>";
+							}
+						?>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+					<?php foreach ( $Table_Details  as $k) {
+								echo "<span class=' d-block py-2 '>".$k['COLUMN_NAME']."</span>";
+								}
+						?>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+					<?php foreach ( $Table_Details  as $k) {
+								echo "<span class=' d-block py-2 '>".$k['DATA_TYPE']."</span>";
+								}
+						?>	
+					</div>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+						<?php foreach($Table_Details  as $k) {
+								echo "<input class=' d-block py-2 ' value=".$k['TABLE_NAME'].">";
+							}
+						?>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+					<?php foreach ( $Table_Details  as $k) {
+								echo "<input class=' d-block py-2 ' value=".$k['COLUMN_NAME'].">";
+								}
+						?>	
+					</div>
+				</td>
+				<td>
+					<div class="d-block">
+					<?php foreach ( $Table_Details  as $k) {
+								echo "<input class=' d-block py-2 ' value=".$k['DATA_TYPE'].">";
+								}
+						?>	
+					</div>	
+					</div>
+				</td>
+			</tr>
+		</table>
+</div>
+<?php }?>
+<a  class="text-decoration-none btn btn-success text-light" href="<?php echo base_url();?>migration">Migrate </a>			
+
+<script>
+function display_table()
+{
+	// var y = x.split('.');
+	// var schema = y[0];
+	// var table = y[1];
+	// alert(table);
+	console.log('schema');
+	$('.table_view').show();
+}
+</script>
